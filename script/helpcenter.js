f73c7e4a9d8ad4c30364a9cb9fa36f3e5e7c3378
vickyFabrics <mosesolawale21@gmail.com>
2020-08-25 10:27:50 +0100

    questions cart and helpcentre added

---
+++ script/helpcenter.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const menu = function(){
+    this.payment = document.querySelector(".payment");
+    this.addToCart = document.querySelector(".addToCart");
+    this.removeFromCart = document.querySelector(".removeFromCart");
+    this.delivery = document.querySelector(".delivery");
+}
+
+menu.prototype = {
+    select(){
+
+    }
+}
